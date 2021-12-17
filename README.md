bec2c9bfb2ce6945486dd8aa3cb4265cda8363b8
Cody Yu <comaniac0422@gmail.com>
2021-12-16 17:45:43 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# artifact-test
+## Github Action Samples
+This is just a repo that I use to experiment Github Actions.
+All experimented actions will be added to this README with descriptions, so feel free to take any of them if that fits your needs.
+
+### Artifacts
+**Workflow File**: `.github/workflows/artifacts.yml`
+
+**Description:** This workflow demonstrates that how to use artifacts at the subsequent jobs in the same run, and the other runs of this workflow.
+
+### Work on any directory in container
+**Workflow File**: `.github/workflows/symbolic.yml`
+
+**Description:** This workflow demonstrates that how to access any directory in the workflow container using symbolic link. The requirement comes when you work on a Github Action in a container, Github Action does not allow you to access the directories outside of the workspace.
+
+### Automatic Create Pull Requests
+**Workflow File**: `.github/workflows/create-pull-request.yml`
+
+**Description:** This workflow demonstrates how to automatically create a pull request using Github Action. This can be used to update submodules regularly, for example.
---
+++ download.py
@@ -0,0 +1,41 @@
+"""A simple script to download the latest Github action artifacts."""
+import json
+import os
+import sys
+
+
+def main(artifact_name, token):
+    """Download the latest artifact."""
+    os.system(
+        f"curl -vLJO -H 'Authorization: token {token}' "
+        "https://api.github.com/repos/comaniac/github-action-samples/actions/artifacts"
+    )
+
+    if not os.path.exists("artifacts"):
+        print("Failed to download artifacts")
+        sys.exit(0)
+
+    with open("artifacts", "r") as filep:
+        data = json.load(filep)
+
+    artifact_url = None
+    for artifact in data["artifacts"]:
+        if artifact["name"] != artifact_name:
+            continue
+        artifact_url = artifact["archive_download_url"]
+        break
+
+    if artifact_url is None:
+        print("Artifact not found")
+        sys.exit(0)
+
+    os.system(f"curl -vLJO -H 'Authorization: token {token}' {artifact_url}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: download_latest_artifact.py <artifact_name> <personal access token>")
+        sys.exit(1)
+
+    main(sys.argv[1], sys.argv[2])
+
